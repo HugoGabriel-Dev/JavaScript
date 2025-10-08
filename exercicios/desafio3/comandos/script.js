87796538b38951bb40d87ce157e865a30a86f036
HugoGabriel <hg116518@gmail.com>
2025-10-08 16:24:17 -0300

    .

--- exercicios/desafio3/comandos/script.js
+++ exercicios/desafio3/comandos/script.js
@@ -1,21 +1,46 @@
-function verificar(){
-    const data = new Date()
-    const ano = data.getFullYear()
-    let fano = document.getElementById('nascimento')
-    let form_ano = fano.value
-    let res = document.querySelector('p#res')
-    if (form_ano.length == 0 || (form_ano > ano)){
-        alert('[ERRO], idade invalida!')
-    }else{
-       let sex = document.getElementsByName('masc')
-       let idade = ano - Number(form_ano)
-       genero = ""
-       if (sex[0].checked){
-        genero = 'Homem'
-       }else if (sex[1].checked){
-        genero = 'Mulher'
-       } 
-       res.textContent = `Você é ${genero} com ${idade} anos`
-    }
+function verificar() {
+  const data = new Date()
+  const ano = data.getFullYear()
+  const fano = document.getElementById('nascimento')
+  const form_ano = Number(fano.value)
+  const res = document.querySelector('#img')
+  const resc = document.getElementById('res')
 
+  if (form_ano == 0 || form_ano > ano) {
+    alert('[ERRO] Idade inválida!')
+  } else {
+    const sex = document.getElementsByName('masc')
+    const idade = ano - Number(form_ano)
+    let genero = ''
+    const img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (sex[0].checked) {
+      genero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        img.setAttribute('src', 'imagens/menino_homem.png')
+      } else if (idade < 21) {
+        img.setAttribute('src', 'imagens/jovem_homem.png')
+      } else if (idade < 50) {
+        img.setAttribute('src', 'imagens/adulto_homem.png')
+      } else {
+        img.setAttribute('src', 'imagens/velho_homem.png')
+      }
+    } else if (sex[1].checked) {
+      genero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        img.setAttribute('src', 'imagens/menina_mulher.png')
+      } else if (idade < 21) {
+        img.setAttribute('src', 'imagens/jovem_mulher.png')
+      } else if (idade < 50) {
+        img.setAttribute('src', 'imagens/adulto_mulher.png')
+      } else {
+        img.setAttribute('src', 'imagens/velho_mulher.png')
+      }
+    }
+    res.textContent = ""
+    resc.innerHTML = `Você é ${genero} com ${idade} anos.`
+    res.appendChild(img)
+    
+  }
 }
