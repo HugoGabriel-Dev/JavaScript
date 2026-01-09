32ceab947c3aeacd557dbae4a1f48acef6c6893a
HugoGabriel <hg116518@gmail.com>
2026-01-09 13:41:26 -0300

    exercicios atualizados

---
+++ estudos alura/01.js
@@ -0,0 +1,18 @@
+let nome = 'hugo gabriel pereira paes'
+console.log(`Olá, ${nome}! Seja bem-vindo(a) ao curso de JavaScript`)
+
+let anoAtual = 2026 
+let anoNascimento = 2007
+console.log(anoAtual - anoNascimento)
+
+let cidade = 'jaboatao dos guararapes'
+let estado = 'pernambuco'
+let pais = 'brasil'
+console.log(`voce mora em ${cidade} - ${estado}, ${pais}`)
+
+let saldo = 0
+console.log(`voce depositou R$200,00 reais, seu saldo agora é de ${saldo = 200}`)
+console.log(`voce sacou R$50,00 reais, seu saldo agora é de ${saldo - 50}`)
+
+let salario = 3000
+console.log(`voce teve um reajuste de 10% seu salario agora é: ${salario = salario + (salario / 10) * 1 }`)
