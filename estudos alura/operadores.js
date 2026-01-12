99944523b086041446fc3acce5ebb92b36d7f0e2
HugoGabriel <hg116518@gmail.com>
2026-01-11 23:10:37 -0300

    .

---
+++ estudos alura/operadores.js
@@ -0,0 +1,24 @@
+const idade = 18
+const maiorIdade = idade >= 18
+console.log('vc é maior de idade?:', maiorIdade)
+
+const nota1 = 7
+const nota2 = 8
+const mediaNotas = (nota1 + nota2) / 2
+console.log('A média do aluno foi:', mediaNotas)
+
+const valorCompra = 35.90 
+const valorPago = 50 
+const troco = valorPago - valorCompra
+console.log('O seu troco foi: ', troco)
+
+const senha1 = 'hugogabriel'
+const senha2 = 'hugogabriel'
+const comparacao = senha1 == senha2
+console.log('As senhas são iguais?: ', comparacao)
+
+const totalFaltas = 30
+const faltasAno = 8
+const PorcentagemFaltas = 25 * totalFaltas / 100
+const faltas = faltasAno > PorcentagemFaltas
+console.log('voce atingiu o total de faltas?: ', faltas)
