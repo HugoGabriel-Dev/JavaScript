65dec03c34b30426e2db9ee0b3c4af55e9e793ba
HugoGabriel <hg116518@gmail.com>
2026-01-13 00:11:27 -0300

    Create condicoes-e-fluxos.js

---
+++ estudos alura/condicoes-e-fluxos.js
@@ -0,0 +1,23 @@
+const idade = 17;
+idade >= 18
+  ? console.log("VOCE PODE BEBER BEBIDA ALCÓLICA")
+  : console.log("VOCE NÃO PODE BEBER BEBIDA ALCÓLICA");
+
+const horaAtual = 10;
+if (horaAtual > 6 && horaAtual < 12) {
+  console.log("bomdia");
+} else if (horaAtual > 12 && horaAtual < 18) {
+  console.log("boatarde");
+} else {
+  console.log("boanoite");
+}
+
+const num = 0;
+if (num >= 1) {
+  console.log("positivo");
+} else if (num < 0) {
+  console.log("negativo");
+} else {
+  console.log("zero");
+}
+
