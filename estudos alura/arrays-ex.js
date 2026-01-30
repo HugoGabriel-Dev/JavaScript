8120166567f20a4e0c28bc157fca36cbc1899673
HugoGabriel <hg116518@gmail.com>
2026-01-30 13:45:55 -0300

    arrays

---
+++ estudos alura/arrays-ex.js
@@ -0,0 +1,44 @@
+let frutas = ["banana", "maça", "uva", "limão", "peira"];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`indice: ${i} ${frutas[i]}`);
+}
+
+frutas.push("melancia"); // adiciona um item a lista no final
+frutas.shift(); // remove o primeiro item
+
+frutas.forEach((valor, indice) => {
+  console.log(`indice: ${indice} ${valor}`);
+});
+
+for (const fruta of frutas) {
+  console.log(fruta);
+}
+
+const notas = [10, 6, 6, 6];
+let soma = 0;
+for (const nota of notas) {
+  soma += nota;
+}
+
+let media = soma / notas.length;
+if (media >= 7) {
+  console.log("aprovado");
+} else {
+  console.log("reprovado");
+}
+
+const nomesDeUsuarios = ["Hugo", "Marcos", "João", "Maria", "Ana"];
+
+nomesDeUsuarios.forEach((valor, indice) => {
+  console.log(`olá ${valor}`);
+});
+
+const precosProdutos = [23, 30, 10, 40.50, 19.90];
+
+console.log("lista antes dos descontos", precosProdutos);
+
+const produtosDesconto = precosProdutos.map((preco) => {
+  return preco - (preco * 10) / 100;
+});
+console.log("lista depois dos descontos", produtosDesconto);
--- estudos alura/arrays.js
+++ estudos alura/arrays.js
@@ -24,3 +24,4 @@ for (let i = 0; i < numeros.length; i++){
     soma += numeros[i]
 }
 console.log(soma)
+
