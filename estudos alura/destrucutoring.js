a4a4ac6941295b458a6aab531398ba3d72b46007
HugoGabriel <hg116518@gmail.com>
2026-02-11 15:36:32 -0300

    estydo

---
+++ estudos alura/destrucutoring.js
@@ -0,0 +1,9 @@
+const pessoa = {
+  nome: "Hugo",
+  idade: 22,
+};
+
+// trocando as chaves de acesso em um objeto com rest e spread
+const { nome: newKey, idade } = pessoa;
+// console.log(newKey)
+
--- estudos alura/spreadRest.js
+++ estudos alura/spreadRest.js
@@ -7,16 +7,30 @@ let hugo2 = { ...hugo };
 hugo2.idade = 30;
 hugo2.profissao = "programador";
 
-console.log(hugo);
-console.log(hugo2);
+// console.log(hugo);
+// console.log(hugo2);
 
 hugo = {
   ...hugo2,
   profissao: "programador senior",
 };
-console.log(hugo);
+// console.log(hugo);
 
 const { nome, ...restante } = hugo;
 
-console.log(nome);
-console.log(restante);
+// console.log(nome);
+// console.log(restante);
+
+// spread e rest usado em arrays
+
+const frutas1 = ["maça", "banana", "uva", "kiwi"];
+const frutas2 = [...frutas1];
+const frutas3 = ["melancia", "morango", "mamao", "ameixa"];
+const frutasTotal = [...frutas1, ...frutas3];
+const [fruta1, fruta2, ...resto] = frutasTotal;
+frutas1.push("pitanga");
+console.log(frutas1);
+console.log(frutas2);
+console.log(frutas3);
+console.log(frutasTotal);
+console.log(fruta1, fruta2, resto);
