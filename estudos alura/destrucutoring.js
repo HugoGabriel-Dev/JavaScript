136ed44d7d6df325f885b6865041f0addd356a6d
HugoGabriel <hg116518@gmail.com>
2026-02-11 23:26:15 -0300

    estydo

--- estudos alura/destrucutoring.js
+++ estudos alura/destrucutoring.js
@@ -1,9 +1,21 @@
 const pessoa = {
   nome: "Hugo",
-  idade: 22,
+  idade: 18,
 };
 
 // trocando as chaves de acesso em um objeto com rest e spread
-const { nome: newKey, idade } = pessoa;
+const { nome, idade } = pessoa;
 // console.log(newKey)
 
+//pega as propriedades do objeto original
+function saudacao({ nome, idade }) {
+  console.log(`Olá,`, nome, `você tem`, idade, "anos");
+}
+saudacao(pessoa);
+
+const frutas = ["uva", "banana", "maça", "morango"];
+
+const [fruta1, fruta2] = frutas;
+
+console.log(fruta1);
+console.log(fruta2);
