ba81092b03184627c2b0e9de9be799cd7ee18521
HugoGabriel <hg116518@gmail.com>
2026-03-11 15:16:15 -0300

    estudos

--- estudos alura/praticando-objs.js
+++ estudos alura/praticando-objs.js
@@ -96,6 +96,63 @@ console.log(arrayValores);
 console.log("Detalhamento:");
 
 for (let [chave, valor] of Object.entries(relatorio)) {
-  const status = valor > 50 ? '(Alerta)' : '(Ok)'
-  console.log(`${chave}: ${valor} ${status}.`)
+  const status = valor > 50 ? "(Alerta)" : "(Ok)";
+  console.log(`${chave}: ${valor} ${status}.`);
 }
+
+// Gerando um relatório de manutenção
+const manutencao = {
+  jan: 2,
+  fev: 2,
+  mar: 5,
+  abr: 5,
+  mai: 0,
+  jun: 7,
+};
+
+let totalDiasParados = 0;
+let mesesComManutecao = 0;
+
+for (let [mes, dias] of Object.entries(manutencao)) {
+  totalDiasParados += dias;
+  if (dias > 0) {
+    mesesComManutecao += 1;
+  }
+}
+
+let mensagem =
+  totalDiasParados > 20
+    ? "Status: Atenção! Acima do limite anual."
+    : "Status: Dentro do limite anual.";
+
+console.log(`Total de dias parados: ${totalDiasParados}`);
+console.log(`Meses com man manutenção: ${mesesComManutecao}`);
+console.log(mensagem);
+
+// Analisando desempenho de estudantes
+const curso = {
+  titulo: "curso",
+  estudantes: [
+    { nome: "Ana", progresso: 85 },
+    { nome: "Carlos", progresso: 40 },
+    { nome: "Juliana", progresso: 72 },
+    { nome: "Pedro", progresso: 60 },
+  ],
+  gerarRelatorio: function () {
+    let totalEstudantes = 0;
+    let somaProgresso = 0;
+    let mediaTotalProgresso = 0;
+    for (let chave of this.estudantes) {
+      console.log(
+        `Nome: ${chave.nome} | Progresso: ${chave.progresso}% | Situação: ${chave.progresso > 70 ? "Aprovado" : "Em andamento"}`,
+      );
+      totalEstudantes++;
+      somaProgresso += chave.progresso;
+    }
+    mediaTotalProgresso = somaProgresso / totalEstudantes;
+    console.log(`Total de estudantes: ${totalEstudantes}`);
+    console.log(`Média geral da turma: ${mediaTotalProgresso}%`);
+  },
+};
+
+curso.gerarRelatorio();
